2fdba0ec5aedaeccf739933608b9221654472d3b
babic <babdan5@gmail.com>
2023-04-20 18:04:31 +0200

    Added colors and theming for the project

--- app/src/main/java/com/example/androidnobelapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androidnobelapp/ui/theme/Theme.kt
@@ -1,21 +1,27 @@
 package com.example.androidnobelapp.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = DarkGreen
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = LightGreen
 
     /* Other default colors to override
     background = Color.White,
@@ -36,6 +42,14 @@ fun AndroidNobelAppTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+        }
     }
 
     MaterialTheme(
